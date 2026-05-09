25ae1a7dfc65102f645361c56098502ae7218649
mohamettapthi75 <mohamettapthi75@gmail.com>
2026-05-10 00:51:47 +0300

    install.sh Termux ku hagaajiyay

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo '#!/usr/bin/env python3' > /tmp/cejiye_temp
-cat cejiye.py >> /tmp/cejiye_temp
-cp /tmp/cejiye_temp $PREFIX/bin/cejiye
+echo '#!/usr/bin/env python3' | cat - cejiye.py > ~/cejiye_temp
+mv ~/cejiye_temp $PREFIX/bin/cejiye
 chmod +x $PREFIX/bin/cejiye
 echo "✅ cejiye si guul leh ayaa la rakibay!"
